e1047433644b3656d3567ea70ea9f8762d80cd5a
your name <santiago4306@gmail.com>
2022-10-03 10:52:16 -0500

    first commit

--- 02-JS-I/homework/tests/JSI.test.js
+++ 02-JS-I/homework/tests/JSI.test.js
@@ -38,7 +38,7 @@ const {
 describe('nuevaString', function() {
   it('Deberia ser un string', function() {
     expect(typeof nuevaString).toBe('string');
-  });
+  }); aaaaaa
 });
 
 describe('nuevoNum', function() {
